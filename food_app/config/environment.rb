7fc84c34defa9b1f4c8c0a847af4db69ba414d25
Zoe Henry <zoe.henry@yale.edu>
2016-07-19 10:43:05 -0400

    removed my sendgrid password, will not send emails without password, but wasn't able to set up env password without heroku

--- food_app/config/environment.rb
+++ food_app/config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   user_name: 'zoehenry', #ENV['SENDGRID_USERNAME'],
-  password: 'P0T4T0EZ', #ENV['SENDGRID_PASSWORD'],
+  password: ENV['SENDGRID_PASSWORD'],
   domain: 'example.com',
   address: 'smtp.sendgrid.net',
   port: 587,
